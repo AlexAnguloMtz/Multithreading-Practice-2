29709da1056a1a9295bd50e5a33202ac7a362e53
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-09-30 14:23:06 -0700

    Added Lombok

--- src/main/java/com/alex/multithreading/line/counter/app/LineCountingResults.java
+++ src/main/java/com/alex/multithreading/line/counter/app/LineCountingResults.java
@@ -1,40 +1,23 @@
 package com.alex.multithreading.line.counter.app;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  *  Immutable data transfer class to transport the obtained results
  *
  *  @author Alex Angulo
  */
+
+@Data
 public class LineCountingResults {
 
     private final String threadName;
+
     private final String fileName;
+
     private final long numberOfLines;
+
     private final long millisecondsPassed;
 
-    public LineCountingResults(String threadName,
-                               String filename,
-                               long numberOfLines,
-                               long millisecondsPassed) {
-        this.threadName = threadName;
-        this.fileName = filename;
-        this.numberOfLines = numberOfLines;
-        this.millisecondsPassed = millisecondsPassed;
-    }
-
-    public String getThreadName() {
-        return threadName;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public long getNumberOfLines() {
-        return numberOfLines;
-    }
-
-    public long getMillisecondsPassed() {
-        return millisecondsPassed;
-    }
 }
