d3d94f563c5560613b216b789c205cbe1fdce4ba
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-09-30 14:26:47 -0700

    Fixed methods names

--- src/main/java/com/alex/multithreading/files/FileLineCounter.java
+++ src/main/java/com/alex/multithreading/files/FileLineCounter.java
@@ -19,13 +19,13 @@ public class FileLineCounter {
 
     public void countLinesInFile(String filename, Counter counter) {
         try (var reader = new BufferedReader(new FileReader(filename))) {
-            doCountLinesInFileWith(reader, counter);
+            countLinesWith(reader, counter);
         } catch (Exception exception) {
             throw new RuntimeException(format(COULD_NOT_READ_FILE, filename));
         }
     }
 
-    private void doCountLinesInFileWith(BufferedReader reader, Counter counter) throws IOException {
+    private void countLinesWith(BufferedReader reader, Counter counter) throws IOException {
         String line = null;
         while ((line = reader.readLine()) != null)
             counter.add(1);
