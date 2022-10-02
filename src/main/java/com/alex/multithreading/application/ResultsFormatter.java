0becbc3eea1be29b5128246c2362ef62d5d616c0
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-10-02 01:34:15 -0700

    Fixed spelling

--- src/main/java/com/alex/multithreading/application/ResultsFormatter.java
+++ src/main/java/com/alex/multithreading/application/ResultsFormatter.java
@@ -5,9 +5,9 @@ import java.util.List;
 import static java.lang.String.format;
 
 public class ResultsFormatter {
-    String formatResults(List<String> resultsOfExcution) {
+    String formatResults(List<String> resultsOfExecution) {
         var builder = new StringBuilder();
-        for (String string : resultsOfExcution) {
+        for (String string : resultsOfExecution) {
             builder.append(format("%s%n", string));
         }
         return builder.toString();
