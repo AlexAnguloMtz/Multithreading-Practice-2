926517b54ff46e902119e9019e1be9e054fe1255
Alex Angulo Martinez <raulalexo100@gmail.com>
2022-10-02 01:10:53 -0700

    Minor refactorings

--- src/main/java/com/alex/multithreading/AbstractExercise.java
+++ src/main/java/com/alex/multithreading/AbstractExercise.java
@@ -1,6 +1,6 @@
 package com.alex.multithreading;
 
-public abstract class AbstractExercise {
+abstract class AbstractExercise {
     static void display(String message) {
         System.out.println(message);
     }
